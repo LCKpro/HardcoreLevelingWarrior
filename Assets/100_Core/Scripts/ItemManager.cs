c32cbde055c187e5b116340f1dfafc5b19810c3c
LCKpro <tjdwls9981@naver.com>
2023-07-21 19:57:48 +0900

    아이템 정보 저장

---
+++ Assets/100_Core/Scripts/ItemManager.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<ItemData> itemDataList;
+
+    public List<ItemData> GetItemData()
+    {
+        if (itemDataList != null)
+        {
+            return itemDataList;
+        }
+
+        GameUtils.Log("ItemData Null");
+
+        return null;
+    }
+
+    public ItemData GetItemDataByUUID(int uuid)
+    {
+        foreach (var item in itemDataList)
+        {
+            if (item.uuid == uuid)
+            {
+                return item;
+            }
+        }
+
+        return null;
+    }
+}
+
+[System.Serializable]
+public class ItemData
+{
+    public int uuid;
+    public string itemName;
+    public string description;
+    public Sprite sprite;
+    public int price;
+}
